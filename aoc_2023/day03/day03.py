8ac84716d9ff54d3677ae990c7a9cde6813a934b
mounasb <mounasb@pm.me>
2023-12-04 23:06:12 +0100

    failed day 3 :'(

---
+++ aoc_2023/day03/day03.py
@@ -0,0 +1,24 @@
+### I'VE GIVEN UP SOLVING THIS ONE :'(
+
+# import re
+# from pprint import pprint as pp
+
+# with open("./day03_input.txt") as f:
+#     lines = f.read().splitlines()
+
+# # pp(lines)
+
+# import ipdb
+
+# ipdb.set_trace()
+
+# for i, line in enumerate(lines):
+#     print(i, "\n", line)
+#     import ipdb
+
+#     ipdb.set_trace()
+#     numbers = re.findall("\d+", line)
+#     symbols = re.findall("[^\w\.]", line)
+
+#     if i != 0 and i != len(lines):
+#         print("here we are")
